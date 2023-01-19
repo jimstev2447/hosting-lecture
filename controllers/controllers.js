7d792b2afe3801289cdf4a786538e1e8f0ee29a9
jimStev2447 <jim.stevenson2447@gmail.com>
2023-01-19 16:16:38 +0000

    more thing

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -3,7 +3,10 @@ const { fetchSnacks, fetchCategoryById } = require('../models/models');
 exports.getSnacks = (req, res, next) => {
   const { category_id } = req.query;
 
-  Promise.all([fetchSnacks(category_id), fetchCategoryById(category_id)])
+  Promise.all([
+    fetchSnacks(category_id),
+    category_id ? fetchCategoryById(category_id) : null,
+  ])
     .then(([snacks]) => {
       res.status(200).send({ snacks });
     })
