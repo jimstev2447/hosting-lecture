0852e5e737f0ebd9ce8a5c330b8514d54d27c7f8
jimStev2447 <jim.stevenson2447@gmail.com>
2023-01-19 08:46:39 +0000

    add .env

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,9 @@
+const ENV = process.env.NODE_ENV || 'development';
+
+const pathToCorrectEnvFile = `${__dirname}/../.env.${ENV}`;
+require('dotenv').config({
+  path: pathToCorrectEnvFile,
+});
 const { Pool } = require('pg');
 
 if (process.env.PGDATABASE === undefined) {
