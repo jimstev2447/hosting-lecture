a39471897c160e47b2d6e2f0b52a6ea1ab9928c0
jimStev2447 <jim.stevenson2447@gmail.com>
2023-01-20 09:58:02 +0000

    make error messages more descriptuive

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const { getSnacks } = require('./controllers/controllers');
 app.get('/api/snacks', getSnacks);
 
 app.use((req, res, next) => {
-  res.status(404).send({ msg: 'Path not found' });
+  res
+    .status(404)
+    .send({
+      msg: 'Path not found please send a correct path, have a look at my wonderful documentation',
+    });
 });
 
 app.use((err, req, res, next) => {
